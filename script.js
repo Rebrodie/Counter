72b37e73d0cf5c42e9e164c7a924e1da11332eef
Malik Bennett <bennettmalik213@gmail.com>
2024-02-20 12:07:05 -0500

    Added Color change

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ let count = 0;
 
 function add() {
   count += 1;
-  text.textContent = "Counter: " + count;
+  text.textContent = count;
+  if (count < 0) {
+    text.style.color = "hsl(17, 99%, 57%)";
+  } else {
+    text.style.color = "";
+  }
 }
 
 function del() {
   count -= 1;
-  text.textContent = "Counter: " + count;
+  text.textContent = count;
+  if (count < 0) {
+    text.style.color = "hsl(17, 99%, 57%)";
+  } else {
+    text.style.color = "";
+  }
 }
 function reset() {
   count = 0;
-  text.textContent = "Counter: " + count;
+  text.textContent = count;
+  if (count < 0) {
+    text.style.color = "hsl(17, 99%, 57%)";
+  } else {
+    text.style.color = "";
+  }
 }
